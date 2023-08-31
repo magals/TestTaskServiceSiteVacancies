21c338df01010660a1672654034b1c860367daf3
magals <itengineer@live.ru>
2023-08-31 10:51:26 +0300

    add: default logger template

--- src/Services/ServiceVacanciesAndResumes/Program.cs
+++ src/Services/ServiceVacanciesAndResumes/Program.cs
@@ -1,4 +1,53 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Serilog;
+using Serilog.Events;
+using Serilog.Expressions;
+using Serilog.Settings.Configuration;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
-app.Run();
+IConfiguration Configuration = new ConfigurationBuilder()
+   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+   .AddEnvironmentVariables()
+   .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    var options = new ConfigurationReaderOptions(typeof(ConsoleLoggerConfigurationExtensions).Assembly, typeof(SerilogExpression).Assembly);
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration, options)
+                    .ReadFrom.Services(services)
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                    .Enrich.WithProperty("ApplicationName", Assembly.GetExecutingAssembly().GetName().Name)
+                    .Enrich.WithProperty("Version", Assembly.GetExecutingAssembly().GetName().Version)
+                    .Enrich.WithProperty("OS Version", RuntimeInformation.OSDescription)
+                    .Enrich.WithProperty("UsedStore", "")
+                    .Enrich.WithMemoryUsage()
+                    .Enrich.WithMachineName()
+                    .Enrich.WithThreadId()
+                    .Enrich.WithEnvironmentName()
+                    .Enrich.WithEnvironmentUserName()
+                    .Enrich.WithClientIp()
+                    .WriteTo.File(@"logs\logs-.txt", LogEventLevel.Information, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 60)
+                    .WriteTo.Console());
+
+
+    var app = builder.Build();
+
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.Logger.Information("Close Program");
+    Log.CloseAndFlush();
+}
